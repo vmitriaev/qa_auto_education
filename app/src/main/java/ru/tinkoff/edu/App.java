ed5da41415514e5f25e81514cca0b428251f9e43
v.mitryaev <v.mitryaev@tinkoff.ru>
2021-09-30 18:29:36 +0300

    pom.xml modified

--- app/src/main/java/ru/tinkoff/edu/App.java
+++ app/src/main/java/ru/tinkoff/edu/App.java
@@ -6,8 +6,7 @@ package ru.tinkoff.edu;
  */
 public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
